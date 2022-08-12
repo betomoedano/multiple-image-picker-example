1eb5863ac3a19060f62d1690f96d7950c6e8e73b
betomoedano <betomoedano@outlook.com>
2022-08-12 19:17:08 -0400

    @follow me! 🤓

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { width } = useWindowDimensions();
+
+  const pickImages = async () => {
+    // No permissions request is necessary for launching the image library
+    setIsLoading(true);
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      // allowsEditing: true,
+      allowsMultipleSelection: true,
+      selectionLimit: 10,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    setIsLoading(false);
+    console.log(result);
+    if (!result.cancelled) {
+      setImages(result.uri ? [result.uri] : result.selected);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={images}
+      renderItem={({ item }) => (
+        <Image
+          source={{ uri: item.uri }}
+          style={{ width: width / 2, height: 250 }}
+        />
+      )}
+      numColumns={2}
+      keyExtractor={(item) => item.uri}
+      contentContainerStyle={{ marginVertical: 50, paddingBottom: 100 }}
+      ListHeaderComponent={
+        isLoading ? (
+          <View>
+            <Text
+              style={{ fontSize: 20, fontWeight: "bold", textAlign: "center" }}
+            >
+              Loading...
+            </Text>
+            <ActivityIndicator size={"large"} />
+          </View>
+        ) : (
+          <Button title="Pick images" onPress={pickImages} />
+        )
+      }
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
